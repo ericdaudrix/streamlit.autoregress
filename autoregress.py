d494a37668ce310b5aaf340dc0561fbf3d52b783
ericdaudrix <159133023+ericdaudrix@users.noreply.github.com>
2025-03-18 16:16:48 +0100

    Update autoregress.py

--- autoregress.py
+++ autoregress.py
@@ -8,10 +8,11 @@ import pandas as pd
 import numpy as np
 
 import skforecast
-
-#from skforecast.ForecasterAutoreg import ForecasterAutoreg  # ✅ Option 1
+st.write("Version de skforecast :", skforecast.__version__)
+st.write("Contenu de skforecast :", dir(skforecast))
+from skforecast.ForecasterAutoreg import ForecasterAutoreg  # ✅ Option 1
 #from skforecast.forecasting.ForecasterAutoreg import ForecasterAutoreg  # ✅ Option 2
-from skforecast.ForecasterAutoreg.forecaster_autoreg import ForecasterAutoreg  # ✅ Option 3
+#from skforecast.ForecasterAutoreg.forecaster_autoreg import ForecasterAutoreg  # ✅ Option 3
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
